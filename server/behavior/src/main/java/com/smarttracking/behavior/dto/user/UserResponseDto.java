0f61d02d9c4c4eb9797513dea0eac7860797a4b8
boss <bosevetrivelram47@gmail.com>
2026-02-16 17:34:44 +0530

    User service, controller, DTO completed

---
+++ server/behavior/src/main/java/com/smarttracking/behavior/dto/user/UserResponseDto.java
@@ -0,0 +1,51 @@
+package com.smarttracking.behavior.dto.user;
+
+public class UserResponseDto {
+
+	private Long userId;
+	private String name;
+	private String email;
+	private String role;
+	private boolean isActive;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}
